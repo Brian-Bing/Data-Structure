b610e9b54804b33fe724756e16fb4c83efd49210
Brian <b22140718@njupt.edn.cn>
2023-10-13 14:28:55 +0800

    Initial Commit

---
+++ 一元多项式的基本操作/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  一元多项式的基本操作
+//
+//  Created by Brian on 2023/10/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
